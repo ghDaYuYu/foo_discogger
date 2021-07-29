a49544cec382234c9b78ef1a2b0eed9ea3211ce1
fooDayuyu@gmail.com <foodayuyu@gmail.com>
2021-07-29 02:12:39 +0200

    Release v2.23-mod18.2

--- track_matching_dialog.h
+++ track_matching_dialog.h
@@ -108,10 +108,7 @@ private:
 		perm_selection.resize(delmask.size());
 		bit_array_bittable are_albums(delmask.size());
 		const size_t max_items = m_tag_writer->get_art_count();
-	
-		//fix crash deleting discogos tracks
-		//size_t csel = get_art_perm_selection(hlist, true, max_items, perm_selection, are_albums);
-		
+
 		m_coord.ListUserCmd(hlist, lsmode::tracks_ui, ID_REMOVE, delmask, pfc::bit_array_bittable(), false);
 		match_message_update(match_manual);
 		return true;

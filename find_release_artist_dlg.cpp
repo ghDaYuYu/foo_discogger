97673693ca42455dd9306b51adfaaa0640aa9bb7
fooDayuyu@gmail.com <foodayuyu@gmail.com>
2021-11-13 09:47:54 +0100

    Find release artist profile dlg

---
+++ find_release_artist_dlg.cpp
@@ -0,0 +1,64 @@
+﻿#include "stdafx.h"
+#include "resource.h"
+
+#include "find_release_artist_dlg.h"
+
+static const GUID guid_cfg_window_placement_find_release_artist_dlg = { 0xbe0df069, 0x3e52, 0x4570, { 0xba, 0xbc, 0x59, 0xbe, 0x75, 0xb8, 0xe0, 0x45 } };
+static cfg_window_placement cfg_window_placement_find_release_artist_dlg(guid_cfg_window_placement_find_release_artist_dlg);
+
+dialog_resize_helper::param CFindReleaseArtistDialog::m_resize_helper_table[] =
+{
+	{IDC_FIND_ARTIST_PROFILE_EDIT, dialog_resize_helper::XY_SIZE},
+	{IDCANCEL,    dialog_resize_helper::XY_MOVE},
+};
+
+CFindReleaseArtistDialog::CFindReleaseArtistDialog(HWND p_parent) :
+	m_resize_helper(m_resize_helper_table, tabsize(m_resize_helper_table), 235, 94, 0, 0)
+{
+    //
+}
+
+void CFindReleaseArtistDialog::UpdateProfile(pfc::string8 name, pfc::string8 profile)
+{
+	pfc::string8 caption;
+	caption << "Artist Profile - " << name;
+	uSetWindowText(m_hWnd, caption);
+	uSetDlgItemText(m_hWnd, IDC_FIND_ARTIST_PROFILE_EDIT, profile);
+}
+
+LRESULT CFindReleaseArtistDialog::OnInitDialog(HWND hWnd, LPARAM lParam)
+{
+	SetIcon(g_discogs->icon);
+	m_resize_helper.process_message(m_hWnd, WM_INITDIALOG, (WPARAM)hWnd, lParam);
+	m_resize_helper.add_sizegrip();
+	cfg_window_placement_find_release_artist_dlg.on_window_creation(m_hWnd, true);
+	ShowWindow(SW_SHOW);
+	return TRUE;
+}
+
+void CFindReleaseArtistDialog::OnDestroy()
+{
+	m_resize_helper.process_message(m_hWnd, WM_DESTROY, 0, 0);
+	cfg_window_placement_find_release_artist_dlg.on_window_destruction(m_hWnd);
+}
+
+void CFindReleaseArtistDialog::OnClose()
+{
+	DestroyWindow();
+}
+
+void CFindReleaseArtistDialog::OnSize(UINT nType, CSize newSize)
+{
+	m_resize_helper.process_message(m_hWnd, WM_SIZE, nType, MAKELPARAM(newSize.cx, newSize.cy));
+	Invalidate();
+}
+
+void CFindReleaseArtistDialog::OnGetMinMaxInfo(LPMINMAXINFO lpMinMaxInfo)
+{
+	m_resize_helper.process_message(m_hWnd, WM_GETMINMAXINFO, 0, (LPARAM)lpMinMaxInfo);
+}
+
+void CFindReleaseArtistDialog::OnCancel(UINT nCode, int nId, HWND hWnd)
+{
+	DestroyWindow();
+}
---
+++ find_release_artist_dlg.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include "resource.h"
+#include "foo_discogs.h"
+
+class CFindReleaseArtistDialog :
+	public CDialogImpl<CFindReleaseArtistDialog>, public CMessageFilter
+{
+public:
+	enum { IDD = IDD_DIALOG_FIND_ARTIST };
+	
+	BEGIN_MSG_MAP(CFindReleaseArtistDialog)
+		MSG_WM_INITDIALOG(OnInitDialog)
+		MSG_WM_DESTROY(OnDestroy)
+		MSG_WM_CLOSE(OnClose)
+		MSG_WM_SIZE(OnSize)
+		MSG_WM_GETMINMAXINFO(OnGetMinMaxInfo)
+		COMMAND_HANDLER_EX(IDCANCEL, BN_CLICKED, OnCancel)
+	END_MSG_MAP()
+
+	CFindReleaseArtistDialog(HWND p_parent);
+
+	~CFindReleaseArtistDialog() {
+		g_discogs->find_release_artist_dialog = nullptr;
+	}
+
+	virtual BOOL PreTranslateMessage(MSG* pMsg) override {
+		return ::IsDialogMessage(m_hWnd, pMsg);
+	}
+
+	void UpdateProfile(pfc::string8 name, pfc::string8 profile);
+
+private:
+
+	LRESULT OnInitDialog(HWND hWnd, LPARAM lParam);
+	void OnDestroy();
+	void OnClose();
+	void OnSize(UINT nType, CSize newSize);
+	void OnGetMinMaxInfo(LPMINMAXINFO lpMinMaxInfo);
+	void OnCancel(UINT nCode, int nId, HWND hWnd);
+
+	dialog_resize_helper m_resize_helper;
+	static dialog_resize_helper::param m_resize_helper_table[];
+};

4940a4475c923e6ddf93f78f9234276217d758a2
fooDayuyu@gmail.com <foodayuyu@gmail.com>
2022-03-18 05:59:11 +0100

    History and artist profile revisions

---
+++ history_oplog.cpp
@@ -0,0 +1,101 @@
+#include "stdafx.h"
+
+#include "db_utils.h"
+#include "history_oplog.h"
+
+bool history_oplog::init(bool enabled, size_t max_items) {
+
+	if (m_enabled == enabled && m_max_items == max_items) return false;
+
+	m_enabled = enabled;
+	m_max_items = max_items;
+
+	zap_vhistory();
+
+	sqldb db;
+	//std::vector<vppair*>out_history = { &m_vres_release_history, &m_vres_artist_history, &m_vres_filter_history };
+	
+	std::map<oplog_type, vppair*> out_history = {
+		{oplog_type::release, &m_vres_release_history},
+		{oplog_type::artist, &m_vres_artist_history},
+		{oplog_type::filter, &m_vres_filter_history}
+	};
+
+	size_t inc = db.recharge_history(kcmdHistoryWashup, max_items, out_history);
+
+	return inc != pfc_infinite;
+
+}
+
+//serves config dialog
+void history_oplog::zap_vhistory() {
+
+	m_vres_release_history.clear();
+	m_vres_artist_history.clear();
+	m_vres_filter_history.clear();
+
+}
+
+bool history_oplog::add_history_row(oplog_type optype, rppair row) {
+	//rppair row_copy = rppair(row);
+
+	if (!get_row_key(optype, row).get_length()) return false;
+
+	vppair& vh = get_history(optype);
+	vppair::iterator fit;
+
+	fit = std::find_if(vh.begin(), vh.end(),
+		[=](const rppair& w) {
+			return get_row_key(optype, w).equals(get_row_key(optype, row));
+		});
+
+	if (!vh.size() || fit == vh.end()) {
+
+		vh.insert(vh.begin(), row);
+		return true;
+	}
+	else {
+		std::iter_swap(vh.begin(), fit);
+	}
+	return false;
+}
+
+bool history_oplog::do_history_menu(oplog_type optype, HWND hwndCtrlParent, pfc::string8 &out) {
+
+
+
+	vppair& vophistory = get_history(optype);
+	if (!vophistory.size()) return false;
+
+	POINT pt= ::GetCursorPos();
+
+	//build menu
+
+	enum { MENU_FIRST_ITEM = 1 };
+	HMENU hMenu = CreatePopupMenu();
+
+	for (auto walk_it = vophistory.begin(); walk_it != vophistory.end(); ++walk_it) {
+
+		const pfc::string8 label = get_menu_label(optype, walk_it._Ptr);
+
+		const pfc::stringcvt::string_os_from_utf8 os_str(label);
+		size_t item_id = MENU_FIRST_ITEM + std::distance(vophistory.begin(), walk_it);
+
+		AppendMenu(hMenu, MF_STRING, item_id, os_str);
+	}
+
+	//display menu
+
+	int cmd = TrackPopupMenu(hMenu, TPM_LEFTALIGN | TPM_TOPALIGN | TPM_RETURNCMD, pt.x, pt.y, 0, hwndCtrlParent, NULL);
+	DestroyMenu(hMenu);
+
+	//return value
+
+	if (cmd) {
+
+		rppair row_h = vophistory.at(--cmd);
+		out = get_row_key(optype, row_h);
+		return out.get_length();
+	}
+	return false;
+}
---
+++ history_oplog.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "db_utils.h"
+
+class history_oplog {
+
+public:
+
+	bool init(bool enabled, size_t max_items);
+	bool do_history_menu(oplog_type optype, HWND hwndParent, pfc::string8& out);
+
+	//serves config dialog
+	void zap_vhistory();
+
+	//from
+	// process release callback on success
+	// search_artist_process_callback on success
+	// get_artist_process_callback
+
+	bool add_history_row(oplog_type optype, rppair row);
+
+private:
+
+	bool m_enabled = false;
+	size_t m_max_items = 0;
+
+	vppair m_vres_release_history;
+	vppair m_vres_artist_history;
+	vppair m_vres_filter_history;
+
+	vppair& get_history(oplog_type optype) {
+		if (optype == oplog_type::artist)		return m_vres_artist_history;
+		else if (optype == oplog_type::release)	return m_vres_release_history;
+		else if (optype == oplog_type::filter)	return m_vres_filter_history;
+		return vppair();
+	}
+
+	pfc::string8 get_row_key(oplog_type optype, const rppair row) {
+		if (optype == oplog_type::artist)		return row.second.first;
+		else if (optype == oplog_type::release)	return row.first.first;
+		else if (optype == oplog_type::filter)	return row.first.first;
+		return "";
+	}
+
+	// menu
+
+	pfc::string8 get_menu_label(oplog_type optype, rppair* row) {
+		if (optype == oplog_type::artist)		return ((PFC_string_formatter() << row->second.first << "\t" << row->second.second).c_str());
+		else if (optype == oplog_type::release)	return ((PFC_string_formatter() << row->first.first << "\t" << row->first.second).c_str());
+		else if (optype == oplog_type::filter)	return row->first.first;
+		return "";
+	}
+};

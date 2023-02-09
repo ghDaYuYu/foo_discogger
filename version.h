7682c026bb7737a12913f812d83e8ed44573e96d
fooDayuyu@gmail.com <foodayuyu@gmail.com>
2023-02-09 21:10:09 +0100

    version 1.0.16

--- version.h
+++ version.h
@@ -1,9 +1,9 @@
-﻿#pragma once
+#pragma once
 #define COMPONENT_NAME "foo_discogger"
 #define COMPONENT_YEAR "2023"
 #define COMPONENT_VERSION_MAJOR 1
 #define COMPONENT_VERSION_MINOR 0
-#define COMPONENT_VERSION_PATCH 15
+#define COMPONENT_VERSION_PATCH 16
 #define COMPONENT_VERSION_SUB_PATCH 0
 
 #define MAKE_STRING(text) #text

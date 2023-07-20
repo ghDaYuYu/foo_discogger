1485f586ccc1d961b7efca7c4ed5feef0f67ab17
fooDayuyu@gmail.com <foodayuyu@gmail.com>
2023-07-20 15:38:48 +0100

    v1.0.18.1

--- version.h
+++ version.h
@@ -1,17 +1,19 @@
-#pragma once
+﻿#pragma once
 #define COMPONENT_NAME "foo_discogger"
 #define COMPONENT_YEAR "2023"
 #define COMPONENT_VERSION_MAJOR 1
 #define COMPONENT_VERSION_MINOR 0
 #define COMPONENT_VERSION_PATCH 18
-#define COMPONENT_VERSION_SUB_PATCH 0
+#define COMPONENT_VERSION_SUB_PATCH 1
+
+#define FIX_VER
 
 #define MAKE_STRING(text) #text
 #define MAKE_COMPONENT_VERSION(major,minor,patch) MAKE_STRING(major) "." MAKE_STRING(minor) "." MAKE_STRING(patch)
 #define MAKE_DLL_VERSION(major,minor,patch,subpatch) MAKE_STRING(major) "." MAKE_STRING(minor) "." MAKE_STRING(patch) "." MAKE_STRING(subpatch)
 #define MAKE_API_SDK_VERSION(sdk_ver, sdk_target) MAKE_STRING(sdk_ver) " " MAKE_STRING(sdk_target)
 
-//"0.1.2"
+//1.0.18 or 1.0.18.1
 #ifdef FIX_VER
 #define FOO_DISCOGGER_VERSION MAKE_DLL_VERSION(COMPONENT_VERSION_MAJOR,COMPONENT_VERSION_MINOR,COMPONENT_VERSION_PATCH,COMPONENT_VERSION_SUB_PATCH)
 #else
